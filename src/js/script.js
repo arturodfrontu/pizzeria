5b8e23ad8614fa0a4b4554d7c54ccc4f9bf2c29f
Artur Trąpczyński <trapczynski.a@gmail.com>
2020-12-10 18:13:05 +0100

    Add rendering

--- src/js/script.js
+++ src/js/script.js
@@ -55,16 +55,26 @@
   class Product{
     constructor(id, data){
       const thisProduct = this;
-      console.log('new product:', thisProduct );
       thisProduct.id = id;
       thisProduct.data = data;
+      thisProduct.renderInMenu();
+      console.log('new Product:', thisProduct);
+    }
+    renderInMenu(){
+      const thisProduct = this;
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      menuContainer.appendChild(thisProduct.element);
     }
   }
 
   const app = {
     initMenu: function(){
+      const testProduct = new Product();
+      console.log('testProduct:', testProduct);
       const thisApp = this;
-      console.log('thisApp.data:', thisApp.data)
+      console.log('thisApp.data: ', thisApp.data);
       for(let productData in thisApp.data.products){
         new Product(productData, thisApp.data.products[productData]);
       }
@@ -75,7 +85,6 @@
 
       thisApp.data = dataSource;
     },
-
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
